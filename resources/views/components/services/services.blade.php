650655f1c8f5e012baa043d91068c79e57e6e11c
asmi046 <asmi046@gmail.com>
2024-10-03 12:50:50 +0300

    Услуги

--- resources/views/components/services/services.blade.php
+++ resources/views/components/services/services.blade.php
@@ -1,9 +1,9 @@
 <div class="services">
     <x-services.card title="Отбеливание" index="1"></x-services.card>
     <x-services.card title="Имплантация" index="2"></x-services.card>
-    <x-services.card title="Плазмолифтинг и лечение заболеваний пародонта" index="3"></x-services.card>
+    <x-services.card title="Плазмолифтинг" index="3"></x-services.card>
     <x-services.card title="Протезирование" index="4"></x-services.card>
-    <x-services.card title="Гигиена " index="5"></x-services.card>
+    <x-services.card title="Гигиена и лечение заболеваний пародонта" index="5"></x-services.card>
     <x-services.card title="Хирургия полости рта" index="6"></x-services.card>
     <x-services.card title="Лечение зубов" index="7"></x-services.card>
     <x-services.card title="Диагностика" index="8"></x-services.card>
