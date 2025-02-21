c803afb0d62dcc211b5126df55dd9bf92a217b7b
Erofeev Dmitry <Kkunkka1@mail.ru>
2025-02-22 00:40:14 +0300

    Добавление elem слайдера, частичная стилизация

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,11 +20,113 @@
         </div>
     </section>
 
+    <section class="services-useful">
+        <div class="container">
+            <h2>Полезная информация</h2>
+            <hr class="gray_hr">
+            <swiper-container space-between="30" slides-per-view="4" speed="500">
+                <swiper-slide>
+                    <a href="#" class="useful-cards">
+                        <div class="useful-cards__item">
+                            <div class="useful-cards__item-img">
+                                <img src="{{ asset('img/useful-1.webp') }}" alt="">
+                            </div>
+                            <div class="useful-cards__item-content">
+                                <div class="useful-cards__item-title">Лечение взрослых и детей под микроскопом</div>
+                                <p class="useful-cards__item-text">
+                                    Лечение кариеса и корневых каналов проводится только под увеличением. Мы используем
+                                    только
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </swiper-slide>
+                <swiper-slide>
+                    <a href="#" class="useful-cards">
+                        <div class="useful-cards__item">
+                            <div class="useful-cards__item-img">
+                                <img src="{{ asset('img/useful-2.webp') }}" alt="">
+                            </div>
+                            <div class="useful-cards__item-content">
+                                <div class="useful-cards__item-title">Отбеливание системы Flash</div>
+                                <p class="useful-cards__item-text">
+                                    Лечение кариеса и корневых каналов проводится только под увеличением. Мы используем
+                                    только
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </swiper-slide>
+                <swiper-slide>
+                    <a href="#" class="useful-cards">
+                        <div class="useful-cards__item">
+                            <div class="useful-cards__item-img">
+                                <img src="{{ asset('img/useful-3.webp') }}" alt="">
+                            </div>
+                            <div class="useful-cards__item-content">
+                                <div class="useful-cards__item-title">Надежная стерилизация всех инструментов</div>
+                                <p class="useful-cards__item-text">
+                                    Лечение кариеса и корневых каналов проводится только под увеличением. Мы используем
+                                    только
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </swiper-slide>
+                <swiper-slide>
+                    <a href="#" class="useful-cards">
+                        <div class="useful-cards__item">
+                            <div class="useful-cards__item-img">
+                                <img src="{{ asset('img/useful-4.webp') }}" alt="">
+                            </div>
+                            <div class="useful-cards__item-content">
+                                <div class="useful-cards__item-title">Фотопротокол</div>
+                                <p class="useful-cards__item-text">
+                                    Лечение кариеса и корневых каналов проводится только под увеличением. Мы используем
+                                    только
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </swiper-slide>
+                <swiper-slide>
+                    <a href="#" class="useful-cards">
+                        <div class="useful-cards__item">
+                            <div class="useful-cards__item-img">
+                                <img src="{{ asset('img/useful-5.webp') }}" alt="">
+                            </div>
+                            <div class="useful-cards__item-content">
+                                <div class="useful-cards__item-title">Лечение взрослых и детей под микроскопом</div>
+                                <p class="useful-cards__item-text">
+                                    Лечение кариеса и корневых каналов проводится только под увеличением. Мы используем
+                                    только
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </swiper-slide>
+            </swiper-container>
+            <div class="reviews_btn_wrapper">
+                <a @click.prevent="prevSlide" class="arrow_button arrow_pred" href="#">
+                    <svg class="sprite_icon">
+                        <use xlink:href="#arrow"></use>
+                    </svg>
+                </a>
+                <a @click.prevent="nextSlide" class="arrow_button arrow_next" href="#">
+                    <svg class="sprite_icon">
+                        <use xlink:href="#arrow"></use>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </section>
+
     <section class="numbers_section">
         <div class="container">
             <h2>Наша клиника в цифрах</h2>
             <hr class="gray_hr">
-            <img class="logo-to-numbers" src="{{ asset('img/icons/logo-to-numbers.svg') }}" alt="Клиника Денталика в цифрах">
+            <img class="logo-to-numbers" src="{{ asset('img/icons/logo-to-numbers.svg') }}"
+                alt="Клиника Денталика в цифрах">
             <x-numbers></x-numbers>
         </div>
     </section>
