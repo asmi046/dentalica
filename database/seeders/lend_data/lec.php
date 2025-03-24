f7c1bf3b0c7b78b87e9e70ec2c96112da8ca4005
Erofeev Dmitry <Kkunkka1@mail.ru>
2025-03-24 23:07:33 +0300

    Добавление 2 ленд_дат

--- database/seeders/lend_data/lec.php
+++ database/seeders/lend_data/lec.php
@@ -1,10 +1,85 @@
 <?php
 $lec_data = [
     [
+        'part' => 'Как проходит лечение?',
+        'picture' => '',
+        'title' => 'Профессиональное удаление отложений:',
+        'sub_title' => '',
+        'content' => file_get_contents(public_path('old_data/lending_data/text/lec_treatment_work_1.html')),
+    ],
+
+    [
+        'part' => 'Как проходит лечение?',
+        'picture' => '',
+        'title' => 'Плазмотерапия (плазмолифтинг):',
+        'sub_title' => '',
+        'content' => file_get_contents(public_path('old_data/lending_data/text/lec_treatment_work_2.html')),
+    ],
+
+    //____________________________________________________________
+    [
         'part' => 'Почему выбирают нас:',
-        'picture' => 'lending_icon/pricel.svg',
+        'picture' => 'lending_icon/vector_para_pro.svg',
         'title' => '',
         'sub_title' => '',
-        'content' => 'Индивидуальный подбор метода имплантации',
+        'content' => 'Современное немецкое оборудовани (VECTOR PARO PRO) для эффективного удаления отложений',
+    ],
+    [
+        'part' => 'Почему выбирают нас:',
+        'picture' => 'lending_icon/plasma_therapy_autoplasma.svg',
+        'title' => '',
+        'sub_title' => '',
+        'content' => 'Плазмотерапия с использованием аутоплазмы для стимуляции регенерации',
+    ],
+    [
+        'part' => 'Почему выбирают нас:',
+        'picture' => 'lending_icon/medic_exp.svg',
+        'title' => '',
+        'sub_title' => '',
+        'content' => 'Врачи-профессионалы с опытом работы от 20 лет и сертифицированным обучением',
+    ],
+    [
+        'part' => 'Почему выбирают нас:',
+        'picture' => 'lending_icon/complex_hirurg_estetic.svg',
+        'title' => '',
+        'sub_title' => '',
+        'content' => 'Комплексный подход, включая хирургическое лечение и эстетическую коррекцию',
+    ],
+
+    //__________________________________________________________________________________
+    [
+        'part' => 'Как подготовиться к процедуре?',
+        'picture' => 'lending_photo/lec-procedure.webp',
+        'title' => '',
+        'sub_title' => '',
+        'content' => [
+            'Подготовка к процедуре:' => file_get_contents(public_path('old_data/lending_data/text/lec_how_procedure_1.html')),
+            'Рекомендации после лечения:' => file_get_contents(public_path('old_data/lending_data/text/lec_how_procedure_2.html'))
+        ],
+    ],
+
+    //_____________________________________________________________________________________
+    [
+        'part' => 'Кому подходит лечение и когда проявлять осторожность?',
+        'picture' => '',
+        'title' => 'Показания:',
+        'sub_title' => '',
+        'content' => file_get_contents(public_path('old_data/lending_data/text/lec_treatment_poc.html')),
+    ],
+    [
+        'part' => 'Кому подходит лечение и когда проявлять осторожность?',
+        'picture' => '',
+        'title' => 'Противопоказания:',
+        'sub_title' => '',
+        'content' => file_get_contents(public_path('old_data/lending_data/text/lec_treatment_prot.html')),
+    ],
+
+    //_____________________________________________________________________________________
+    [
+        'part' => 'Что вы получите после лечения?',
+        'picture' => '',
+        'title' => '',
+        'sub_title' => '',
+        'content' => file_get_contents(public_path('old_data/lending_data/text/lec_what_treatment.html')),
     ],
 ];
--- database/seeders/lend_data/white.php
+++ database/seeders/lend_data/white.php
@@ -1,10 +1,94 @@
 <?php
 $white_data = [
     [
-        'part' => 'Почему выбирают нас:',
-        'picture' => 'lending_icon/pricel.svg',
+        'part' => 'Технология и методика процедуры',
+        'picture' => '',
         'title' => '',
         'sub_title' => '',
-        'content' => 'Индивидуальный подбор метода имплантации',
+        'content' => 'Применяет холодный LED-свет, который не перегревает зубы',
+    ],
+
+    [
+        'part' => 'Технология и методика процедуры',
+        'picture' => '',
+        'title' => '',
+        'sub_title' => '',
+        'content' => 'Обеспечивает равномерное и устойчивое осветление эмали до 10 оттенков за 1,5 часа',
+    ],
+
+    [
+        'part' => 'Технология и методика процедуры',
+        'picture' => '',
+        'title' => '',
+        'sub_title' => '',
+        'content' => 'Возможна индивидуальная настройка лампы для зубов с повышенной чувствительностью',
+    ],
+
+    //________________________________________________________
+    [
+        'part' => 'Почему выбирают нас:',
+        'picture' => 'lending_icon/white_safety.svg',
+        'title' => 'Безопасность',
+        'sub_title' => '',
+        'content' => 'Современное оборудование, сертифицированные материалы, исключение ожогов  тканей зуба и слизистой',
+    ],
+    [
+        'part' => 'Почему выбирают нас:',
+        'picture' => 'lending_icon/white_long_effect.svg',
+        'title' => 'Устойчивый эффект',
+        'sub_title' => '',
+        'content' => 'Эффект до 3 лет при соблюдении рекомендаций',
+    ],
+    [
+        'part' => 'Почему выбирают нас:',
+        'picture' => 'lending_icon/white_affo_price.svg',
+        'title' => 'Доступная цена',
+        'sub_title' => '',
+        'content' => 'Прямая работа с поставщиками',
+    ],
+    [
+        'part' => 'Почему выбирают нас:',
+        'picture' => 'lending_icon/white_exp_staff.svg',
+        'title' => 'Опытный персонал ',
+        'sub_title' => '',
+        'content' => 'Врачи с опытом более 10 лет и специальным обучением',
+    ],
+
+    //________________________________________________________
+
+    [
+        'part' => 'Как происходит процедура',
+        'picture' => 'lending_photo/white_procedure.webp',
+        'title' => '',
+        'sub_title' => '',
+        'content' => file_get_contents(public_path('old_data/lending_data/text/white_how_procedure.html')),
+    ],
+
+    //___________________________________________________
+
+    [
+        'part' => 'Кому подходит процедура?',
+        'picture' => '',
+        'title' => 'Показания:',
+        'sub_title' => '',
+        'content' => file_get_contents(public_path('old_data/lending_data/text/white_treatment_poc.html')),
+    ],
+
+    [
+        'part' => 'Кому подходит процедура?',
+        'picture' => '',
+        'title' => 'Противопоказания:',
+        'sub_title' => '',
+        'content' => file_get_contents(public_path('old_data/lending_data/text/white_treatment_prot.html')),
+    ],
+
+    //_____________________________________
+
+    [
+        'part' => 'Как сохранить эффект отбеливания?',
+        'picture' => '',
+        'title' => 'Ключевые рекомендации:',
+        'sub_title' => '',
+        'content' => file_get_contents(public_path('old_data/lending_data/text/white_safe_effect.html')),
     ],
 ];
