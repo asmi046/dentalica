ad9fc0c2bed0a9c10c751d77e5fdc75eb3917617
asmi046 <asmi046@gmail.com>
2026-04-03 15:45:40 +0300

    Опечатка

--- resources/views/components/blog-in-main.blade.php
+++ resources/views/components/blog-in-main.blade.php
@@ -7,13 +7,13 @@
                     <img class="logo-to-numbers" src="{{ asset('img\logo_transparent.svg') }}" alt="">
                     <div class="elelemt">
                         <div class="number">{{ $texts_count }}</div>
-                        <p class="text_comment">Статей и текстовых маткриалов по вопросам стоматологии</p>
+                        <p class="text_comment">Статей и текстовых материалов по вопросам стоматологии</p>
                         <a href="{{ route('blog.texts') }}" class="button">Читать</a>
                     </div>
 
                     <div class="elelemt">
                         <div class="number">{{ $videos_count }}</div>
-                        <p class="text_comment">Познавательных видео на тему ухода за зубами и подготовке к имплантации
+                        <p class="text_comment">Познавательных видео на тему ухода за зубами и подготовки к имплантации
                         </p>
                         <a href="{{ route('blog.videos') }}" class="button">Смотреть</a>
                     </div>
