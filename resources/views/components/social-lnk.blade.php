3c7de1d4c4f1e0993ad755216ba936e5c757e7f1
asmi046 <asmi046@gmail.com>
2026-09-01 14:33:12 +0300

    Убран инстаграмм

--- resources/views/components/social-lnk.blade.php
+++ resources/views/components/social-lnk.blade.php
@@ -1,5 +1,5 @@
 <div class="social-lnk">
-    @if( !empty($contacts['vk']))
+    @if (!empty($contacts['vk']))
         <a href="{{ $contacts['vk'] }}" class="link vk_lnk">
             <svg class="sprite_icon">
                 <use xlink:href="#vk_icon"></use>
@@ -7,23 +7,23 @@
         </a>
     @endif
 
-    @if( !empty($contacts['tg']))
-    <a href="{{ $contacts['tg'] }}" class="link tg_lnk">
-        <svg class="sprite_icon">
-            <use xlink:href="#tg_icon"></use>
-        </svg>
-    </a>
+    @if (!empty($contacts['tg']))
+        <a href="{{ $contacts['tg'] }}" class="link tg_lnk">
+            <svg class="sprite_icon">
+                <use xlink:href="#tg_icon"></use>
+            </svg>
+        </a>
     @endif
 
-    @if( !empty($contacts['ws']))
-    <a href="{{ $contacts['ws'] }}" class="link ws_lnk">
-        <svg class="sprite_icon">
-            <use xlink:href="#ws_icon"></use>
-        </svg>
-    </a>
+    @if (!empty($contacts['ws']))
+        <a href="{{ $contacts['ws'] }}" class="link ws_lnk">
+            <svg class="sprite_icon">
+                <use xlink:href="#ws_icon"></use>
+            </svg>
+        </a>
     @endif
 
-    {{-- @if( !empty($contacts['ok']))
+    {{-- @if (!empty($contacts['ok']))
     <a href="{{ $contacts['ok'] }}" class="link ok_lnk">
         <svg class="sprite_icon">
             <use xlink:href="#ok_icon"></use>
@@ -31,11 +31,11 @@
     </a>
     @endif --}}
 
-    @if( !empty($contacts['in']))
+    {{-- @if (!empty($contacts['in']))
     <a href="{{ $contacts['in'] }}" class="link in_lnk">
         <svg class="sprite_icon">
             <use xlink:href="#in_icon"></use>
         </svg>
     </a>
-    @endif
+    @endif --}}
 </div>
